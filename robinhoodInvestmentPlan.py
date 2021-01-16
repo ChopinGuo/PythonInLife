dce3e0c63684d391db826b72a662ad122a41e644
ChopinGuo <guoshaopan@pku.edu.cn>
2021-01-15 22:48:18 -0600

    Update robinhoodInvestmentPlan.py

--- robinhoodInvestmentPlan.py
+++ robinhoodInvestmentPlan.py
@@ -17,13 +17,17 @@ data1 = Data[0]
 print(data1[1:4])
 fobj.close()
 
+def equityInPlan(share, portfolioValue, planInvestment, planDailyInvestment, remainingDays):
+    planDailyInvestment = planInvestment / 20
+    return (portfolioValue - planDailyInvestment * remainingDays)  * share
+
 portfolioValue = float(input("Enter the portfolio value: $"))
-remainingDays = int(input("Enter the remaining work days of this month: "))
 planInvestment = 900
 planDailyInvestment = planInvestment / 20
+remainingDays = int(input("Enter the remaining work days of this month: "))
 print("Current Cash in Plan is: ${:.2f}".format(planDailyInvestment * remainingDays))
-planEthEquity = (portfolioValue - planDailyInvestment * remainingDays) / 9
-planBitcoinEquity = 2 * planEthEquity
+planEthEquity = equityInPlan(1/9, portfolioValue, planInvestment, planDailyInvestment, remainingDays)
+planBitcoinEquity = equityInPlan(2/9, portfolioValue, planInvestment, planDailyInvestment, remainingDays)
 print("Current Bitcoin Equity in Plan is: ${:.2f}".format(planBitcoinEquity))
 print("Current ETH Equity in Plan is: ${:.2f}".format(planEthEquity))
 
@@ -39,12 +43,46 @@ if remainingDays == 0 or option == 'y':
     profit = portfolioValue - balanceLastMonth
     print("The profit in this month is: ${:.2f}".format(profit))
     print("The profit ratio in the  past month: {:.2f}%".format(profit * 100 / balanceLastMonth))
-    currentQqqEquity = float(input("Enter the current QQQ equity: $"))
-    print("QQQ equity in plan: ${:.2f}".format(2 * planEthEquity))
-    print("You should trade ${:.2f}".format(2 * planEthEquity - currentQqqEquity))
+    planSPY = equityInPlan(4/9, portfolioValue, planInvestment, planDailyInvestment, remainingDays)
+    planQQQ = equityInPlan(2/9, portfolioValue, planInvestment, planDailyInvestment, remainingDays)
+    print("SPY equity in plan: ${:.2f}".format(planSPY))
+    print("QQQ equity in plan: ${:.2f}".format(planQQQ))
     currentSpyEquity = float(input("Enter the current SPY equity: $"))
-    print("SPY equity in plan: ${:.2f}".format(4 * planEthEquity))
     print("You should trade ${:.2f}".format(4 * planEthEquity - currentSpyEquity))
+    currentQqqEquity = float(input("Enter the current QQQ equity: $"))
+    print("You should trade ${:.2f}".format(2 * planEthEquity - currentQqqEquity))
     end = str(input("Press Any Key to Exit."))
 else:
     end = str(input("Press Any Key to Exit."))
+
+# portfolioValue = float(input("Enter the portfolio value: $"))
+# remainingDays = int(input("Enter the remaining work days of this month: "))
+# planInvestment = 900
+# planDailyInvestment = planInvestment / 20
+# print("Current Cash in Plan is: ${:.2f}".format(planDailyInvestment * remainingDays))
+# planEthEquity = (portfolioValue - planDailyInvestment * remainingDays) / 9
+# planBitcoinEquity = 2 * planEthEquity
+# print("Current Bitcoin Equity in Plan is: ${:.2f}".format(planBitcoinEquity))
+# print("Current ETH Equity in Plan is: ${:.2f}".format(planEthEquity))
+
+# currentBitcoinEquity = float(input("Enter the current Bitcoin Equity: $"))
+# print("You should trade ${:.2f}".format(planBitcoinEquity - currentBitcoinEquity))
+# currentEthEquity = float(input("Enter the current ETH Equity: $"))
+# print("You should trade ${:.2f}".format(planEthEquity - currentEthEquity))
+# option = str(input("Do you wnt to calculate the profit in this month? (y/n)"))
+
+# if remainingDays == 0 or option == 'y':
+#     # balanceLastMonth = float(input("Enter the balance in the last month: $"))
+#     balanceLastMonth = 11889.73
+#     profit = portfolioValue - balanceLastMonth
+#     print("The profit in this month is: ${:.2f}".format(profit))
+#     print("The profit ratio in the  past month: {:.2f}%".format(profit * 100 / balanceLastMonth))
+#     print("SPY equity in plan: ${:.2f}".format(4 * planEthEquity))
+#     print("QQQ equity in plan: ${:.2f}".format(2 * planEthEquity))
+#     currentSpyEquity = float(input("Enter the current SPY equity: $"))
+#     print("You should trade ${:.2f}".format(4 * planEthEquity - currentSpyEquity))
+#     currentQqqEquity = float(input("Enter the current QQQ equity: $"))
+#     print("You should trade ${:.2f}".format(2 * planEthEquity - currentQqqEquity))
+#     end = str(input("Press Any Key to Exit."))
+# else:
+#     end = str(input("Press Any Key to Exit."))
